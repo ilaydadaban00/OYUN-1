e06240af1cb1c6acb9809c9951612c5d887f1315
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-28 12:42:30 +0300

    Add files via upload

---
+++ Oyun2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oyun2
+{
+    public partial class Form1 : Form
+    {
+
+        Oyuncu oyuncu1;
+        int sayac=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+
+            lblBilgi.Text = oyuncu1.BilgiYaz();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            string rId = random.Next(1000, 10000).ToString();
+
+            //kurucu metotlar new anahtarı ile oluşturma aşamasında çalışır.
+            oyuncu1 = new Oyuncu(rId, txtAd.Text, txtSoyad.Text, txtPosta.Text, 100, DateTime.Now, false);
+        }
+
+        private void btnVur_Click(object sender, EventArgs e)
+        {
+            
+            vur(oyuncu1);
+            lblBilgi.Text = oyuncu1.BilgiYaz();
+
+            sayac++;
+           
+            if (sayac < 6)
+            {
+                vur(oyuncu1);
+              
+            }
+            if (oyuncu1.defans <= 0)
+            {
+                MessageBox.Show("kazandınız");
+                btnVur.Enabled = false;
+            }
+            if(oyuncu1.defans > 0 && sayac == 6)
+            {
+                MessageBox.Show("oyuncu kazandı");
+                btnVur
+            }
+
+        }
+        public void vur(Oyuncu vurus)
+        {
+           
+            Random r = new Random();
+            oyuncu1.defans = oyuncu1.defans - r.Next(0, 6);
+            
+        }
+        
+
+    }
+}
---
+++ Oyun2/Oyuncu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oyun2
+{
+         public class Oyuncu
+    {
+        public string Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Posta { get; set; }
+        public double Puan { get; set; }
+        public DateTime KayitTarih { get; set; }    
+        public bool Aktif {  get; set; }
+        public int defans { get; set; }
+
+        //kurucu metot tanımlama.
+        //Sadece kurucu metotlarda metod adı sınıf ile aynıdır
+        //Kurucu metot tanımlandıktan sonra parameterler yazılır
+        public Oyuncu(string id, string ad, string soyad, string posta, double puan,DateTime kayitTarih,bool aktifmi)
+        {
+            Id = id;
+            Ad = ad;
+            Soyad = soyad;
+            Posta = posta;
+            Puan = puan;
+            KayitTarih = kayitTarih;
+            Aktif = aktifmi;
+        }
+
+        public string BilgiYaz()
+        {
+            return "ID :\t\t" + Id  + "\nAd:\t\t" + Ad + "\nSoyad :\t\t" + Soyad+ "\nKayıt Tarihi:\t" + KayitTarih;
+        }
+        public void DefansArttır()
+        {
+            Random random = new Random();
+            defans=random.Next(10,20);
+            
+        }
+
+    }
+}
